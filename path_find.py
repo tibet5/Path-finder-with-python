6e221bca8f37f65d099665212048cde8d77c724f
tibet5 <59174238+tibet5@users.noreply.github.com>
2021-02-19 23:58:41 +0300

    Create path_find.py

---
+++ path_find.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+print('write the name and type ex "file_name file", "foler_name folder", "script_name script"')
+
+name,type_name = input().split()
+
+
+os.chdir('/')
+
+for root, dirs, files in os.walk('/'): 
+    if type_name == 'file':
+
+        for file in files:
+
+            if str(file) == name:
+                print(root+'/'+str(file))
+
+    if type_name == 'folder':
+
+        for folder in dirs:
+
+            if str(folder) == name:
+                #print(root+'/'+str(folder))
+                result[] = root+'/'+str(folder)
+
+    if type_name == 'script':
+
+        for file in files:
+
+            if file.endswith('.sh') and str(file) == name:
+                print(root+'/'+str(file))
+
